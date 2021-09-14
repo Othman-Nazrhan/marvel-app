9e49178b7d1611060bf2a50c83c2c87c2c72ba50
Othman-Nazrhan <Othman-Nazrhan>
2021-09-14 15:04:42 +0100

    Updates

---
+++ precache-manifest.e976f9fe5b2d5ee9d24febb53d9cd2da.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c52a5a6f48b5f11850a759b6405505",
+    "url": "/marvel-app/index.html"
+  },
+  {
+    "revision": "7e8104ff82355a9c1ebf",
+    "url": "/marvel-app/static/css/2.21158041.chunk.css"
+  },
+  {
+    "revision": "32b455ed33767ad68168",
+    "url": "/marvel-app/static/css/main.602e99a6.chunk.css"
+  },
+  {
+    "revision": "7e8104ff82355a9c1ebf",
+    "url": "/marvel-app/static/js/2.7e4c94ed.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/marvel-app/static/js/2.7e4c94ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b455ed33767ad68168",
+    "url": "/marvel-app/static/js/main.bdbbae07.chunk.js"
+  },
+  {
+    "revision": "0000f5be5dfd12392f51",
+    "url": "/marvel-app/static/js/runtime-main.a9621f1e.js"
+  },
+  {
+    "revision": "859dd8b6d58a48077843eaef8fff1efe",
+    "url": "/marvel-app/static/media/bg-noise-texture.859dd8b6.png"
+  },
+  {
+    "revision": "a23e804265f4b1cba5cf60cee515ddf3",
+    "url": "/marvel-app/static/media/deadpool.a23e8042.png"
+  },
+  {
+    "revision": "ed509e33519b210c1606921e78501bf9",
+    "url": "/marvel-app/static/media/griffes-droit.ed509e33.png"
+  },
+  {
+    "revision": "e578fea6021d2318d02c9f353a3f72ad",
+    "url": "/marvel-app/static/media/griffes-gauche.e578fea6.png"
+  },
+  {
+    "revision": "248392c9463115f69738082636d79a7e",
+    "url": "/marvel-app/static/media/ironman.248392c9.png"
+  },
+  {
+    "revision": "6a1254d8df2375f4ae56aee5ae8bab09",
+    "url": "/marvel-app/static/media/spiderman.6a1254d8.png"
+  },
+  {
+    "revision": "6c4150e99b299fe7d7c78e15e31bb377",
+    "url": "/marvel-app/static/media/wolverine.6c4150e9.png"
+  },
+  {
+    "revision": "738ca667ed66ccf0fc7072dff28ab603",
+    "url": "/marvel-app/static/media/xmen.738ca667.png"
+  }
+]);
